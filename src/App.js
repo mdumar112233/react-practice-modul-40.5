2bc14cbefeeadfa6131bb32aa1fa17ceca11dd03
mduumar112233 <mdumarfaruk187326@gmail.com>
2021-03-13 11:10:23 +0600

    added new code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+// import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+// import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import { useState } from 'react';
+import MealDetail from './MealDetail/MealDetail';
+import MealFinder from './MealFinder/MealFinder';
 
 function App() {
+  // const [likeColor, setLikeColor] = useState('');
+  // const handleClick = () => {
+  //   const color = likeColor ? '' : 'primary';
+  //   setLikeColor(color);
+  // }
   return (
     <div>
-
+      {/* <AccessAlarmIcon></AccessAlarmIcon>
+      <ThumbUpIcon onClick={() => setLikeColor(likeColor ? '' : 'primary')} color={likeColor}></ThumbUpIcon> */}
+      <MealDetail></MealDetail>
+      <MealFinder></MealFinder>
     </div>
   );
 }
